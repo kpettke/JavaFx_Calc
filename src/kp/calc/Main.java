975d8b0df97db66cd09fad80c59232c62e8852fd
Krzysztof Pettke <krzysiek.piotr@gmail.com>
2021-02-16 22:36:00 +0100

    GUI done

--- src/kp/calc/Main.java
+++ src/kp/calc/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("resources/calc.fxml"));
         primaryStage.setTitle("asdasd");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 300, 475));
         primaryStage.show();
     }
 
--- src/kp/calc/controllers/CalcController.java
+++ src/kp/calc/controllers/CalcController.java
@@ -1,4 +1,25 @@
 package kp.calc.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
 public class CalcController {
+
+    @FXML
+    private Label calculations;
+
+    public void onMouseClick(MouseEvent mouseEvent) {
+
+        Button button = (Button) mouseEvent.getSource();
+        String number = button.getText();
+
+        insertNumber(number);
+    }
+
+    public void insertNumber (String number){
+        calculations.setText(calculations.getText() + number);
+
+    }
 }
